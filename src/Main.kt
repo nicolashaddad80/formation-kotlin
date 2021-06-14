90ed1040e0990b1b640846ceb882ca32ccfa008f
Nicolas HADDAD <nicolas.haddad@renault.com>
2021-06-14 15:28:24 +0200

    feat: exercise 7 lambda

--- src/Main.kt
+++ src/Main.kt
@@ -18,7 +18,8 @@ fun function5(name:String, number:Int = 42, toUperCase:Boolean = false):String {
 //Exo6
 //Fonction d'extention
 
-fun MutableList<Int>.swap(index1:Int,index2:Int){
+fun <T> MutableList<T>.swap(index1:Int,index2:Int){
+    require(index1 in 0 until this.size && index2 in 0 until this.size)
     val tmp = this[index1]
     this[index1] = this[index2]
     this[index2] = tmp
@@ -56,8 +57,25 @@ fun main() {
     /*println(function5("titi",55, true))*/
 
     //Exo 6
-    val maMutableList = mutableListOf(1, 2, 3, 4, 5)
+    /*val maMutableList = mutableListOf(1, 2, 3, 4, 5)
     println("before Swap: $maMutableList")
     maMutableList.swap(2,3)
     println("after Swap: $maMutableList")
+    maMutableList.swap(2,30)
+    */
+
+     // Exo 7
+    val calculateGrade = {x:Int ->
+        when(x){
+        in 0 ..40 -> "fail"
+        in 41 .. 70 -> "pass"
+        in 71 .. 100 -> "distinction"
+        else -> "other"
+        }
+    }
+
+    println(calculateGrade(40))
+    println(calculateGrade(70))
+    println(calculateGrade(100))
+    println(calculateGrade(1000))
 }
