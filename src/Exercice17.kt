6e2f9ea87c398c2414d349fe11d75c22f9ef9cb2
Nicolas HADDAD <nicolas.haddad@renault.com>
2021-06-15 15:10:13 +0200

    exercise17: collection summary exercise

---
+++ src/Exercice17.kt
@@ -0,0 +1,39 @@
+
+    private val letterScores = mapOf(
+        'A' to 1,
+        'E' to 1,
+        'I' to 1,
+        'O' to 1,
+        'U' to 1,
+        'L' to 1,
+        'N' to 1,
+        'R' to 1,
+        'S' to 1,
+        'T' to 1,
+        'D' to 2,
+        'G' to 2,
+        'B' to 3,
+        'C' to 3,
+        'M' to 3,
+        'P' to 3,
+        'F' to 4,
+        'H' to 4,
+        'W' to 4,
+        'Y' to 4,
+        'K' to 5,
+        'J' to 8,
+        'X' to 8,
+        'Q' to 10,
+        'Z' to 10
+        )
+
+    private fun getLetterScore(c: Char) = letterScores.getValue(c)
+
+    fun getScore(s: String) =  s.replace("\\s".toRegex(), "").toUpperCase().toList().map {getLetterScore(it) }.sum()
+
+
+
+fun main() {
+
+    println(getScore(" tu es bien ehh"))
+}
