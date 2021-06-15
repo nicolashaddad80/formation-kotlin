bf1691701a6041d4e60fd25247b92c686eb75d94
Nicolas HADDAD <nicolas.haddad@renault.com>
2021-06-15 10:37:39 +0200

    exercice 11 : companion object

--- src/UserC.kt
+++ src/UserC.kt
@@ -1,8 +1,16 @@
-class UserC private constructor (val nickName : String
+class UserC private constructor (
+    val nickName : String
 ){
 
     companion object{
         fun newSubscribingUser(email: String) = UserC(email.substringBefore("@"))
         fun newFacebookUser(accountId: Int) = UserC("fb:$accountId")
     }
+}
+
+fun main(){
+    val subscribingUser = UserC.newSubscribingUser("bob@gmail.com")
+    val facebookUser = UserC.newFacebookUser(4)
+    println(subscribingUser.nickName)
+    println(facebookUser.nickName)
 }
