3e2a784c159f23f7fa43c07d83d5ad8e0abc380d
Nicolas HADDAD <nicolas.haddad@renault.com>
2021-06-15 12:14:30 +0200

    feat:Exercise 13: Interface propreties

---
+++ src/Exercice13.kt
@@ -0,0 +1,15 @@
+interface Information {
+    val email: String
+    val nickname: String
+        get() = email.substringBefore('@')
+}
+
+data class UserD(
+    val id:Int ,
+    override val email: String
+):Information{
+}
+fun main(){
+    val user = UserD(42, "jerome@kotlin.com")
+    println("$user::nickname ${user.nickname}")
+}
--- src/exercise12.kt
+++ src/exercise12.kt
@@ -10,15 +10,15 @@ object ApiManager:Network {
     var baseUrl: String? = null
     set(value) {
         println("Disconnecting")
-        isConnected  =false
+        disconnect {}
         field = value
     }
     private var isConnected: Boolean = false
 
     override fun connect(success: (message: String) -> Unit, fail: (errorMessage: String) -> Unit) {
         if (baseUrl != null){
-            println("Connecting")
             isConnected = true
+            println("Connecting")
             success("Success")
         } else {
             fail("Not found")
@@ -37,15 +37,17 @@ object ApiManager:Network {
 }
 
 fun main() {
-    ApiManager.sendHello({message:String, code: Int -> println("$message:$code")}, {message:String, code: Int -> println("$message:$code")})
-    ApiManager.baseUrl = "www.google.fr"
-    ApiManager.sendHello({message:String, code: Int -> println("Sending1$message:$code")}, {message:String, code: Int -> println("$message:$code")})
-    ApiManager.baseUrl = "www.google.com"
-    ApiManager.connect({message:String-> println("Connection: $message")}, {message:String -> println("Connection: $message")})
-    ApiManager.sendHello({message:String, code: Int -> println("Sending2:$message:$code")}, {message:String, code: Int -> println("$message:$code")})
-    ApiManager.baseUrl = "www.google.ca"
-    ApiManager.sendHello({message:String, code: Int -> println("Sending3:$message:$code")}, {message:String, code: Int -> println("$message:$code")})
-    ApiManager.baseUrl = null
-    ApiManager.sendHello({message:String, code: Int -> println("Sending4:$message:$code")}, {message:String, code: Int -> println("$message:$code")})
+    ApiManager.run{
+        sendHello({message:String, code: Int -> println("$message:$code")}, {message:String, code: Int -> println("$message:$code")})
+        baseUrl = "www.google.fr"
+        sendHello({message:String, code: Int -> println("Sending1$message:$code")}, {message:String, code: Int -> println("$message:$code")})
+        baseUrl = "www.google.com"
+        connect({message:String-> println("Connection: $message")}, {message:String -> println("Connection: $message")})
+        sendHello({message:String, code: Int -> println("Sending2:$message:$code")}, {message:String, code: Int -> println("$message:$code")})
+        baseUrl = "www.google.ca"
+        sendHello({message:String, code: Int -> println("Sending3:$message:$code")}, {message:String, code: Int -> println("$message:$code")})
+        baseUrl = null
+        sendHello({message:String, code: Int -> println("Sending4:$message:$code")}, {message:String, code: Int -> println("$message:$code")})
+    }
 
 }
