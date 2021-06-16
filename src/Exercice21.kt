07c5c30cc8343ede90060f5cb609c123532b0e61
Nicolas HADDAD <nicolas.haddad@renault.com>
2021-06-16 11:04:35 +0200

    Exercise21: smart Casting

--- src/Exercice21.kt
+++ src/Exercice21.kt
@@ -1,12 +1,14 @@
 interface Expr
 class Num(val value: Int) :Expr
 class Sum(val left: Expr, val right: Expr) : Expr
+class Sub(val left: Expr, val right: Expr) : Expr
 
 
 fun eval(expr: Expr): Int =
     when(expr){
         is Num -> expr.value
         is Sum -> eval(expr.left) + eval(expr.right)
+        is Sub -> eval(expr.left) - eval(expr.right)
         else -> throw (IllegalArgumentException())
     }
 
@@ -14,6 +16,6 @@ fun main() {
     val a = Num(10)
     val b = Num(5)
     val c = Sum(a,Num(6))
-    val result = eval(Sum(c,b))
+    val result = eval(Sub(c,b))
     println(result)
 }
